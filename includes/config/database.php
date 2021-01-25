2a58e88e80cebe48f587af2e50bcd2340898f990
Moises Jimenez Macias <MoisesJimenez@Yandex.com>
2021-01-24 19:53:06 -0600

    DB Conexion
    +Agregada la Conexion a la Bases de Datos
    +Agregada la seccion Index del CRUD
    +Agregada la seccion de crear del CRUD

---
+++ includes/config/database.php
@@ -0,0 +1,12 @@
+<?php
+
+function conectarDB() : mysqli{
+    $db = mysqli_connect('localhost', 'root', 'root', 'bienes_raices');
+
+    if(!$db){
+        echo 'No fue posible conectarse a la Base de Datos';
+        exit;
+    }
+
+    return $db;
+}
--- includes/templates/header.php
+++ includes/templates/header.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bienes Raices</title>
-    <link rel="stylesheet" href="build/css/app.css">
+    <link rel="stylesheet" href="/build/css/app.css">
 </head>
 
 <body>
@@ -13,14 +13,14 @@
         <div class="contenedor contenido-header">
             <div class="barra">
                 <a href="/">
-                    <img src="build/img/logo.svg" alt="Logotipo de Bienes Raices">
+                    <img src="/build/img/logo.svg" alt="Logotipo de Bienes Raices">
                 </a>
                 <div class="mobile-menu">
-                    <img src="build/img/barras.svg" alt="Icono Menu Responsive">
+                    <img src="/build/img/barras.svg" alt="Icono Menu Responsive">
                 </div>
 
                 <div class="derecha">
-                    <img src="build/img/dark-mode.svg" alt="Modo Dark" class="dark-mode-boton">
+                    <img src="/build/img/dark-mode.svg" alt="Modo Dark" class="dark-mode-boton">
                     <nav class="navegacion">
                         <a href="nosotros.php">Nosotros</a>
                         <a href="anuncios.php">Anuncios</a>

56c1515d17a5543a3055882a28ffc5c07fea0387
Moises Jimenez Macias <MoisesJimenez@Yandex.com>
2021-01-24 20:09:47 -0600

    POST METHOD
    +Agregado metodo POST para enviar fomrulario

--- admin/propiedades/crear.php
+++ admin/propiedades/crear.php
@@ -3,7 +3,11 @@
     require '../../includes/config/database.php';
     $db = conectarDB();
 
-    var_dump($db);
+    if($_SERVER['REQUEST_METHOD'] === 'POST'){
+        $titulo = $_POST['titulo'];
+        $precio = $_POST['precio'];
+    }
+
 
     require '../../includes/funciones.php';  
     incluirTemplate('header');
@@ -13,33 +17,33 @@
         <h1>Crear</h1>
         <a href="/admin/" class="boton boton-verde">Volver</a>
 
-        <form class="formulario">
+        <form class="formulario" method="POST" action="/admin/propiedades/crear.php">
             <fieldset>
                 <legend>Informacion General</legend>
                 <label for="titulo">Titulo:</label>
-                <input type="text" id="titulo" placeholder="Titulo de la Propiedad">
+                <input type="text" name="titulo" id="titulo" placeholder="Titulo de la Propiedad">
 
                 <label for="precio">Precio:</label>
-                <input type="number" id="precio" placeholder="Precio de la Propiedad">
+                <input type="number" name="precio" id="precio" placeholder="Precio de la Propiedad">
 
                 <label for="imagen">Imagen:</label>
-                <input type="file" id="imagen" accept="image/jpeg, image/png">
+                <input type="file" name="imagen" id="imagen" accept="image/jpeg, image/png">
 
                 <label for="descripcion">Descripción:</label>
-                <textarea id="descripcion"></textarea>
+                <textarea id="descripcion" name="descripcion"></textarea>
             </fieldset>
 
             <fieldset>
                 <legend>Informacion de la Propiedad</legend>
                 
                 <label for="habitaciones">Habitaciones:</label>
-                <input type="number" id="habitaciones" placeholder="Ej: 3" min="1" max="9">
+                <input type="number" name="habitaciones" id="habitaciones" placeholder="Ej: 3" min="1" max="9">
 
                 <label for="wc">Baños:</label>
-                <input type="number" id="wc" placeholder="Ej: 3" min="1" max="9">
+                <input type="number" name="wc" id="wc" placeholder="Ej: 3" min="1" max="9">
 
                 <label for="estacionamiento">Estacionamiento:</label>
-                <input type="number" id="estacionamiento" placeholder="Ej: 3" min="1" max="9">
+                <input type="number" name="estacionamiento" id="estacionamiento" placeholder="Ej: 3" min="1" max="9">
             </fieldset>
 
             <fieldset>

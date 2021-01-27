1c034f3486a0f184eafc31f86b87836430e36189
Moises Jimenez Macias <MoisesJimenez@Yandex.com>
2021-01-27 02:15:11 -0600

    Read Finalizado
    +Agregado listado de propiedades en admin/index.php

--- admin/index.php
+++ admin/index.php
@@ -1,7 +1,20 @@
 <?php
+
+    //Importar la conexion
+    require '../includes/config/database.php';
+    $db = conectarDB();
     
+    //escribir el query
+    $query = "SELECT * FROM propiedades";
+    
+    //consular la base de datos
+    $resultadoConsulta = mysqli_query($db, $query);
+    
+
+    //Muetra mensaje condicional
     $resultado = $_GET['resultado'] ?? null;
 
+    //Incluye un template
     require '../includes/funciones.php';  
     incluirTemplate('header');
 ?>
@@ -14,5 +27,38 @@
 
         <a href="/admin/propiedades/crear.php" class="boton boton-verde">Nueva Propiedad</a>
     </main>
+    
+    <table class="propiedades">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Titulo</th>
+                <th>Imagen</th>
+                <th>Precio</th>
+                <th>Acciones</th>
+            </tr>
+        </thead>
 
-<?php incluirTemplate('footer'); ?>
+        <tbody><!--.Mostrar los resultados-->
+        <?php while($propiedad = mysqli_fetch_assoc($resultadoConsulta)): ?>
+            <tr>
+                <td><?php echo $propiedad['id']; ?></td>
+                <td><?php echo $propiedad['titulo']; ?></td>
+                <td><img src="/imagenes/<?php echo $propiedad['imagen']; ?>" alt="Casa en la Playa" class="imagen-tabla"></td>
+                <td>$<?php echo $propiedad['precio']; ?></td>
+                <td>
+                    <a href="#"class="boton boton-rojo-block">Eliminar</a>
+                    <a href="#"class="boton boton-amarillo-block">Actualizar</a>
+                </td>
+            </tr>
+        <?php endwhile; ?>
+        </tbody>
+    </table>
+
+<?php
+    //cerrar la conexion
+    mysqli_close($db);
+
+    //incluir template
+    incluirTemplate('footer'); 
+ ?>

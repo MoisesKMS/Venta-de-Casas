66d9a386ab651a5373cb8f3ec6fba238f649c12f
Moises Jimenez Macias <MoisesJimenez@Yandex.com>
2021-01-27 21:35:10 -0600

    Anuncios Dinamicos
    +Se muestran los anuncions desde la base de datos

--- index.php
+++ index.php
@@ -29,102 +29,12 @@
 
     <section class="seccion contenedor">
         <h2>Casas y Depas en Venta</h2>
-        <div class="contenedor-anuncios">
-            <div class="anuncio">
-                <picture>
-                    <source srcset="build/img/anuncio1.webp" type="image/webp" />
-                    <source srcset="build/img/anuncio1.jpg" type="image/jpeg" />
-                    <img loading="lazy" src="build/img/anuncio1.jpg" alt="Anuncio">
-                </picture>
-                <div class="contenido-anuncio">
-                    <h3>Casa de Lujo en el Lago</h3>
-                    <p>Casa en el Lago con excelente vista, acabados de lujo a un Excelente Precio</p>
-                    <p class="precio">$3,000,000</p>
-                    <ul class="iconos-caracteristicas">
-                        <li>
-                            <img class="icono" loading="lazy" src="build/img/icono_wc.svg" alt="Icono WC">
-                            <p>3</p>
-                        </li>
-                        <li>
-                            <img class="icono" loading="lazy" src="build/img/icono_estacionamiento.svg" alt="Icono Estacionamiento">
-                            <p>3</p>
-                        </li>
-                        <li>
-                            <img class="icono" loading="lazy" src="build/img/icono_dormitorio.svg" alt="Icono Habitaciones">
-                            <p>4</p>
-                        </li>
-                    </ul>
-                    <a href="anuncio.php" class="boton-amarillo-block">
-                        Ver Propiedad
-                    </a>
-                </div>
-                <!--.contenido-anuncio-->
-            </div>
-            <!--.anuncio-->
-            <div class="anuncio">
-                <picture>
-                    <source srcset="build/img/anuncio2.webp" type="image/webp" />
-                    <source srcset="build/img/anuncio2.jpg" type="image/jpeg" />
-                    <img loading="lazy" src="build/img/anuncio2.jpg" alt="Anuncio">
-                </picture>
-                <div class="contenido-anuncio">
-                    <h3>Casa terminado de Lujo</h3>
-                    <p>Casa con diseño moderno, así como tecnología inteligente y amueblada</p>
-                    <p class="precio">$2,000,000</p>
-                    <ul class="iconos-caracteristicas">
-                        <li>
-                            <img class="icono" loading="lazy" src="build/img/icono_wc.svg" alt="Icono WC">
-                            <p>3</p>
-                        </li>
-                        <li>
-                            <img class="icono" loading="lazy" src="build/img/icono_estacionamiento.svg" alt="Icono Estacionamiento">
-                            <p>3</p>
-                        </li>
-                        <li>
-                            <img class="icono" loading="lazy" src="build/img/icono_dormitorio.svg" alt="Icono Habitaciones">
-                            <p>4</p>
-                        </li>
-                    </ul>
-                    <a href="anuncio.php" class="boton-amarillo-block">
-                        Ver Propiedad
-                    </a>
-                </div>
-                <!--.contenido-anuncio-->
-            </div>
-            <!--.anuncio-->
-            <div class="anuncio">
-                <picture>
-                    <source srcset="build/img/anuncio3.webp" type="image/webp" />
-                    <source srcset="build/img/anuncio3.jpg" type="image/jpeg" />
-                    <img loading="lazy" src="build/img/anuncio3.jpg" alt="Anuncio">
-                </picture>
-                <div class="contenido-anuncio">
-                    <h3>Casa con Alverca</h3>
-                    <p>Casa con alberca y acabados de lujo en la ciudad, excelente oportunidad</p>
-                    <p class="precio">$3,000,000</p>
-                    <ul class="iconos-caracteristicas">
-                        <li>
-                            <img class="icono" loading="lazy" src="build/img/icono_wc.svg" alt="Icono WC">
-                            <p>3</p>
-                        </li>
-                        <li>
-                            <img class="icono" loading="lazy" src="build/img/icono_estacionamiento.svg" alt="Icono Estacionamiento">
-                            <p>3</p>
-                        </li>
-                        <li>
-                            <img class="icono" loading="lazy" src="build/img/icono_dormitorio.svg" alt="Icono Habitaciones">
-                            <p>4</p>
-                        </li>
-                    </ul>
-                    <a href="anuncio.php" class="boton-amarillo-block">
-                        Ver Propiedad
-                    </a>
-                </div>
-                <!--.contenido-anuncio-->
-            </div>
-            <!--.anuncio-->
-        </div>
-        <!--.contenedor-anuncios-->
+
+        <?php
+            $limite = 3;
+            include 'includes/templates/anuncios.php'
+        ?>
+
         <div class="alinear-derecha">
             <a href="anuncios.php" class="boton-verde">Ver Todas</a>
         </div>

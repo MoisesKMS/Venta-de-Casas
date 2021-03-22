d0b0b8f64f657a1b1003b0dece12f24fc72cafaa
ScarKMS <MoisesJimenez@Yandex.com>
2021-03-21 22:18:40 -0600

    3.0.0 Versión base del Sitio Bienes Raices usando el patron Model View Controller

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
-const { src, dest, watch , parallel } = require('gulp');
+const { src, dest, watch, parallel } = require('gulp');
 const sass = require('gulp-sass');
 const autoprefixer = require('autoprefixer');
-const postcss    = require('gulp-postcss')
+const postcss = require('gulp-postcss')
 const sourcemaps = require('gulp-sourcemaps')
 const cssnano = require('cssnano');
 const concat = require('gulp-concat');
@@ -26,42 +26,42 @@ function css() {
         .pipe(postcss([autoprefixer(), cssnano()]))
         .pipe(postcss([autoprefixer()]))
         .pipe(sourcemaps.write('.'))
-        .pipe( dest('./build/css') );
+        .pipe(dest('./public/build/css'));
 }
 
 
 function javascript() {
     return src(paths.js)
-      .pipe(sourcemaps.init())
-      .pipe(concat('bundle.js')) // final output file name
-      .pipe(terser())
-      .pipe(sourcemaps.write('.'))
-      .pipe(rename({ suffix: '.min' }))
-      .pipe(dest('./build/js'))
+        .pipe(sourcemaps.init())
+        .pipe(concat('bundle.js')) // final output file name
+        .pipe(terser())
+        .pipe(sourcemaps.write('.'))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(dest('./public/build/js'))
 }
 
 function imagenes() {
     return src(paths.imagenes)
-        .pipe(cache(imagemin({ optimizationLevel: 3})))
-        .pipe(dest('build/img'))
+        .pipe(cache(imagemin({ optimizationLevel: 3 })))
+        .pipe(dest('./public/build/img'))
         //.pipe(notify({ message: 'Imagen Completada'}))
-        ;
+    ;
 }
 
 function versionWebp() {
     return src(paths.imagenes)
-        .pipe( webp() )
-        .pipe(dest('build/img'))
+        .pipe(webp())
+        .pipe(dest('./public/build/img'))
         //.pipe(notify({ message: 'Imagen Completada'}))
-        ;
+    ;
 }
 
 
 function watchArchivos() {
-    watch( paths.scss, css );
-    watch( paths.js, javascript );
-    watch( paths.imagenes, imagenes );
-    watch( paths.imagenes, versionWebp );
+    watch(paths.scss, css);
+    watch(paths.js, javascript);
+    watch(paths.imagenes, imagenes);
+    watch(paths.imagenes, versionWebp);
 }
-  
-exports.default = parallel(css, javascript,  imagenes, versionWebp, watchArchivos ); 
+
+exports.default = parallel(css, javascript, imagenes, versionWebp, watchArchivos);
